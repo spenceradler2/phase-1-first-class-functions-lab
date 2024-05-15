51efc61f0d51228fccac2570f7020f2c3cac043b
Spencer <spencer.adler2@gmail.com>
2024-05-15 11:52:42 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers   = function (driver = ['Antonia', 'Nuru', 'Amari', 'Mo']){
+const drivercopy =[...driver];
+    return drivercopy.splice(0,2);
+}
+
+const returnLastTwoDrivers   = function (drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']){
+    const driverscopy =[...drivers];
+    return driverscopy.splice(2,4);
+    }
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers ]
+
+
+function createFareMultiplier(integer){
+    return (x) => x * integer;
+}
+
+function fareDoubler(fare){
+const more = createFareMultiplier(2);
+return more(fare)
+}
+
+function fareTripler(fares){
+    const more = createFareMultiplier(3);
+    return more(fares)
+    }
+
+function  selectDifferentDrivers(drivers, callback){
+ return callback(drivers)
+}
+
+// selectDifferentDrivers(drivers, returnFirstTwoDrivers)
